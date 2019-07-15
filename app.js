a23747705bf22d39e85fc415ae2cc4f892e4a208
FunctionJoo <joojy0914@gmail.com>
2019-07-15 16:45:10 +0900

    commit

--- app.js
+++ app.js
@@ -1,7 +1,54 @@
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
 
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+res.sendFile(path.join(__dirname + '/index.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+
+router.get('/external', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/external.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet01', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/stylesheet01.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet02', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/stylesheet02.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet03', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/stylesheet03.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet04', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/stylesheet04.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheetNo', function (req, res) {
+res.sendFile(path.join(__dirname + '/views/stylesheetNo.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+    
+
+
+app.use('/', router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
